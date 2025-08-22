82b08297c4fbbf2702b39d74a4e96d7759cf3e04
LuanAlmeida022 <fretsaio@gmail.com>
2025-08-22 23:41:07 +0000

    navios diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 
+#define tam 10
+#define navio 3
+
 int main(){
   
 
     // Letras das colunas (A até J, horizontal)
-    char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
+    char colunas[tam]= {'A','B','C','D','E','F','G','H','I','J'};
 
     // Números das linhas (1 até 10, vertical)
-    int linhas[10] = {1,2,3,4,5,6,7,8,9,10};
+    int linhas[tam] = {1,2,3,4,5,6,7,8,9,10};
 
     // Tabuleiro inicializado com 0
-    int tabuleiro[10][10] = {0};
+    int tabuleiro[tam][tam] = {0};
 
-    // Posicionar navio horizontal (linha 3, coluna 5)
     
-    int linhaH = 2, colunaH = 4; 
-    for(int i = 0; i < 3; i++ ){
+    // Navio diagonal principal (↘)
+    int linhaDP = 1, colunaDP = 1; 
+    for(int i = 0; i < navio; i++ ){
+        tabuleiro[linhaDP + i][colunaDP + i] = 3;
+    }
+
+    // Navio diagonal secundária (↙)
+    int linhaDS = 0, colunaDS = 9; 
+    for(int i = 0; i < navio; i++ ){
+        tabuleiro[linhaDS + i][colunaDS - i] = 3;
+    }
+    
+    // Navio horizontal
+    int linhaH = 5, colunaH = 4; 
+    for(int i = 0; i < navio; i++ ){
 
         tabuleiro[linhaH][colunaH + i] = 3;
     }
 
-    // Posicionar navio vertical (linha 3, coluna 5)
-    
-    int linhaV = 5, colunaV = 7; 
-    for(int i = 0; i < 3; i++ ){
+    // Navio vertical 
+    int linhaV = 5, colunaV = 1; 
+    for(int i = 0; i < navio; i++ ){
 
         tabuleiro[linhaV + i][colunaV] = 3;
     }
 
 
     // Exibir cabeçalho de colunas (A–J)
+    printf(" *** Tabuleiro Batalha Naval *** \n");
     printf("   ");
     for (int j = 0; j < 10; j++){
         printf("%c ", colunas[j]);
@@ -37,9 +52,10 @@ int main(){
     printf("\n");
 
     // Exibir tabuleiro completo
-    for (int i = 0; i < 10; i++) {
+
+    for (int i = 0; i < tam; i++) {
         printf("%2d ", linhas[i]); // imprime número da linha (1–10)
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < tam; j++) {
             printf("%d ", tabuleiro[i][j]); // imprime cada posição
         }
         printf("\n");
